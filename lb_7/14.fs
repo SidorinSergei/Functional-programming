770167193374bec98adec5484aafc4c89d423909
SidorinSergei <95975705+SidorinSergei@users.noreply.github.com>
2022-04-24 23:03:42 +0300

    Create 14.fs

---
+++ lb_7/14.fs
@@ -0,0 +1,21 @@
+let rec vvod n = 
+    match n with
+       | 0-> []
+       | _ -> 
+          let Head = System.Convert.ToInt32(System.Console.ReadLine())
+          let Tail = vvod (n-1)
+          Head::Tail
+// Вывод элемнмтов
+let rec vivod = function    
+    |[]->0
+    |h::tail->
+        printfn $"{h}"
+        vivod tail
+let Fun list = List.length(List.filter(fun x->x%2=0)list)
+
+[<EntryPoint>]
+let main argv =
+    printfn $"Введите количество элементов"
+    let list = vvod (System.Convert.ToInt32(System.Console.ReadLine()))    
+    printfn $"ответ:{list|>Fun}"
+    0
